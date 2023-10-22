fd5408e47c1776bb8d8b92b38551f35fb73d13a4
Jason Harris <lord.jtharris@gmail.com>
2023-10-21 21:11:24 -0500

    Added an additional display of ascii values with spaces between

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main()
 {
 	string stringValueConcat;
+	string stringConcatWhitespace;
 	int stringValueTotal = 0;
 	string input;
 
@@ -15,8 +16,11 @@ int main()
 		int castLetterToInt = (int)c;
 		stringValueTotal += castLetterToInt;
 		stringValueConcat += to_string(castLetterToInt);
+		stringConcatWhitespace += to_string(castLetterToInt) + " ";
 	}
 
 	cout << "Total value of the string is: " << stringValueTotal << endl;
-	cout << "A concatinated version of all of the values in the string would look like: " << stringValueConcat;
+	cout << "A concatinated version of all of the values in the string would look like: " << stringValueConcat << endl;
+	cout << "The same value, but with spaces between each individual ASCII value: " << stringConcatWhitespace << endl;
+	return 0;
 }
